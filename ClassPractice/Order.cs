fcfced7f07c5dd82520fd7e138c7093b2988b6dc
sosuisen <hidekazu.kubota@gmail.com>
2023-12-29 21:07:27 +0900

    add practice2

--- ClassPractice/Order.cs
+++ ClassPractice/Order.cs
@@ -2,20 +2,9 @@ namespace ClassPractice
 {
     internal class Order
     {
-        public int OrderId { get; set; }
-        public string CustomerName { get; set; }
-        public DateTime OrderDate { get; set; }
-
-        public Order(int orderId, string customerName, DateTime dateTime)
-        {
-            OrderId = orderId;
-            CustomerName = customerName;
-            OrderDate = dateTime;
-        }
-
         public void DisplayOrder()
         {
-            Console.WriteLine($"Order ID: {OrderId}, Customer: {CustomerName}, Date: {OrderDate.ToShortDateString()}");
+            Console.WriteLine("Order ID: 7");
         }
     }
 }
--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -4,16 +4,8 @@ namespace ClassPractice
     {
         static void Main(string[] args)
         {
-            Order order = new Order(1, "Alan Kay", DateTime.Now);
+            Order order = new Order();
             order.DisplayOrder();
-
-            // Same result as above
-            OrderPrimary order2 = new(1, "Alan Kay", DateTime.Now);
-            order2.DisplayOrder();
-
-            // Use var keyword to instantiate an object
-            var order3 = new OnlineOrder(2, "Alan Turing", DateTime.Now, "England");
-            order3.DisplayOrder();
         }
     }
 }
