33f018f9861f4854b53aa5a431d58d057cad5a31
sosuisen <hidekazu.kubota@gmail.com>
2023-12-29 21:08:58 +0900

    practice1

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -4,16 +4,10 @@ namespace ClassPractice
     {
         static void Main(string[] args)
         {
-            Order order = new Order(1, "Alan Kay", DateTime.Now);
-            order.DisplayOrder();
-
-            // Same result as above
-            OrderPrimary order2 = new(1, "Alan Kay", DateTime.Now);
-            order2.DisplayOrder();
-
-            // Use var keyword to instantiate an object
-            var order3 = new OnlineOrder(2, "Alan Turing", DateTime.Now, "England");
-            order3.DisplayOrder();
+            DateTime date = new DateTime(2023, 12, 31);
+            Console.WriteLine(date);
+            date = date.AddDays(1);
+            Console.WriteLine(date);
         }
     }
 }
