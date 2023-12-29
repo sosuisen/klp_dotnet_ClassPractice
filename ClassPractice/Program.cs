65d47b7263a0a65b8dd66bbff664bf06c6b29e0e
sosuisen <hidekazu.kubota@gmail.com>
2023-12-29 22:57:07 +0900

    add second object

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -6,6 +6,9 @@ namespace ClassPractice
         {
             Order order = new Order(1, "Alan Kay", DateTime.Now);
             order.DisplayOrder();
+
+            Order order2 = new Order(2, "Ted Nelson", DateTime.Now.AddDays(1));
+            order2.DisplayOrder();
         }
     }
 }
