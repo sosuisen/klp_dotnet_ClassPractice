e3ad717851b8f26171dfee9d066530d7fa090371
sosuisen <hidekazu.kubota@gmail.com>
2023-12-30 01:30:45 +0900

    add kadai3

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -4,7 +4,7 @@ namespace BookPractice
     {
         static void Main(string[] args)
         {
-            var book = new Book("Cixin Liu, Ken Liu", "The Three-Body Problem", 400);
+            var book = new Book("Donald E. Knuth", "Literate Programming", 384);
             book.DisplayBook();
 
             var book2 = new ElectronicBook("Cixin Liu, Ken Liu", "The Three-Body Problem", 400, 2.9);
